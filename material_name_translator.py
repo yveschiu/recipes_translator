9b91e8a061d2670363d096a0e5f7feb67695300b
yveschiu <yves.s.c.chiu@gmail.com>
2019-02-28 17:26:04 +0800

    mnt modified

--- material_name_translator.py
+++ material_name_translator.py
@@ -1,4 +1,5 @@
 import pickle
+import time
 from googletrans import Translator
 
 
@@ -12,16 +13,22 @@ def translate_ch_to_en(material_name):
 
 
 err_list = []
-for recipe_num, recipe in enumerate(recipes_list[:100]):
+for recipe_num, recipe in enumerate(recipes_list):
     for material_num, material in enumerate(recipe["materials"]):
         ch = material.get("material_name")
         try:
             en = translate_ch_to_en(ch)
+            material["material_name_en"] = en
+            ok_msg = "recipe_num {} material_num {}: {} is successfully downloaded.".format(recipe_num, material_num, ch)
+            print(ok_msg)
         except:
             err_msg = (recipe_num, material_num, ch)
+            print("{} translation is failed".format(err_msg))
             err_list.append(err_msg)
 
-        material["material_name_en"] = en
+        time.sleep(0.005)
+
+
 
 # pickle a variable to a file
 file = open('translated_recipes.pkl', 'wb')
